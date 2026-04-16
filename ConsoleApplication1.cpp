a2f065881039fe2530c6a866910bd403f1e5ad03
161142272 <66869173+161142272@users.noreply.github.com>
2026-04-16 22:20:14 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, n;
+    for (int i = 0; i < t; i++)
+    {
+        char sp;
+        cin >> sp;
+        cin >> n;
+
+        if (sp == 'I')
+        {
+            int* number = new int[n];
+            for (int j = 0; j < n; j++)
+            {
+                cin >> *(number + j);
+            }
+
+            int sum;
+            for (int j = 0; j < n; j++)
+            {
+                sum += *(number + j);
+            }
+            cout << sum / n << endl;
+        }
+
+        if (sp == 'C')
+        {
+            char* number = new char[n];
+            for (int j = 0; j < n; j++)
+            {
+                cin >> *(number + j);
+            }
+
+            char max;
+            max = *number;
+            for (int j = 0; j < n; j++)
+            {
+                if (max < *(number + j))
+                {
+                    max = *(number + j);
+                }
+            }
+            cout << max << endl;
+
+        }
+
+        if (sp == 'F')
+        {
+            float* number = new float[n];
+            for (int j = 0; j < n; j++)
+            {
+                cin >> *(number + j);
+            }
+
+            int min;
+            min = *number;
+            for (int j = 0; j < n; j++)
+            {
+                if (min > *(number + j))
+                {
+                    min = *(number + j);
+                }
+            }
+            cout << min << endl;
+        }
+    }
+
+}
